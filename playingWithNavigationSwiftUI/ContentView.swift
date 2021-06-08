d880e7e9265b8e54bb07f82da723d88635336418
Harriette Berndes <harrietteberndes@BRIGHT-OS-01.local>
2021-06-08 12:02:49 +0100

    added programmatic nav using isActive parameter

--- playingWithNavigationSwiftUI/ContentView.swift
+++ playingWithNavigationSwiftUI/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var wantToShowPage3: Bool = false
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -15,6 +18,11 @@ struct ContentView: View {
                 Text("hello same page")
                 NavigationLink(destination: Text("hello page 2 hopefully")) {
                     Text("go to page 2")
+                }
+                NavigationLink(destination: Text("hopefully a page displayed due to programmatically triggering navigation"), isActive: $wantToShowPage3) {
+                    Button("go to page 3") {
+                        self.wantToShowPage3 = true
+                    }
                 }
             }
         }
